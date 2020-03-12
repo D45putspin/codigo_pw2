2322fb143bbb8680c3f08fb29ec19814b571e875
Tiago Nunes Moreira <t14g0@protonmail.com>
2020-03-12 02:11:57 +0000

    inde alt1

--- aula 3/index.php
+++ aula 3/index.php
@@ -21,7 +21,7 @@ $livro1 = new Book("harry potter", "J K Rolling",28.99);
 $livro2 = new Book("Biblia", "Deus",90.99);
 $livro3 = new Book("MOMO", "Thomas Mann",18.99);
 $livro4 = new Book("ensaio sobre a cegueira", "saramgo",38.99);
-
+$carrinhoCompras = array($livro1,$livro2,$livro3,$livro4);
 ?>
 
 
@@ -31,4 +31,10 @@ $livro4 = new Book("ensaio sobre a cegueira", "saramgo",38.99);
         <title> PHP OBJECT </title>
     </head>
     <body>
-        <h1 >carrinho de compras</h1>
+        <h1>carrinho de compras</h1>
+        <?php
+        for(i=0;i<$carrinhoCompras.count;i++){
+            echo "titulo:".$carrinhoCompras[i]->getTitle();
+            
+        }
+        
